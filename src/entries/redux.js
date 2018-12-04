1c5190d6220ff3efc8e86f3dca0af56201bb4b0c
agdwm <almudenaguerraswm@gmail.com>
2018-12-04 11:35:34 +0100

    Created the app 'store' and added the 'webtools' enhancer

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -1,1 +1,41 @@
-console.log('ola ke ase');
+import { createStore } from 'redux';
+
+const $form = document.getElementById('form');
+$form.addEventListener('submit', handleSubmit);
+
+function handleSubmit (e) {
+	e.preventDefault();
+	// 'FormData' is a Native Browser's Api that will receive by param a DOM element.
+	// FormData will save the data of the form in order we can serialize them or send them to the server.
+	const data = new FormData($form);
+	const title = data.get('title');
+	console.log(title);
+}
+
+const initialState = [
+	{
+		"title": "Despacito"
+	},
+	{
+		"title": "One more time"
+	},
+	{
+		"title": "Echame la culpa"
+	}
+]
+
+const store = createStore(
+	(state) => state,
+	initialState,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+const $container = document.getElementById('playlist');
+const playlist = store.getState();
+playlist.forEach(el => {
+	const template = document.createElement('p');
+	template.textContent = el.title;
+	$container.appendChild(template);
+})
+
+console.log(store.getState())
