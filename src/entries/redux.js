7103f28f5697a46f45ff6cfd8f00fe76ca76d42f
agdwm <almudenaguerraswm@gmail.com>
2018-12-11 18:50:32 +0100

    Modified the file 'api.json' in order to set different ids

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -7,7 +7,7 @@ function handleSubmit (e) {
 	e.preventDefault();
 	const data = new FormData($form);
 	const title = data.get('title');
-	//1- UI lanza una acción en evento submit que es recogida por el reducer
+	//1- UI lanza una ACTION en evento submit que es recogida por el REDUCER
 	store.dispatch({
 		type: 'ADD_SONG',
 		payload: {title} //title: title
@@ -26,7 +26,7 @@ const reducer = function(state, action) {
 		case 'ADD_SONG':
 			//3- El reducer actualiza el State
 			return [...state, action.payload]
-		default: 
+		default:
 			return state
 	}
 }
@@ -37,11 +37,21 @@ const store = createStore(
 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-const $container = document.getElementById('playlist');
-const playlist = store.getState();
+function render() {
+  const $container = document.getElementById('playlist');
+  const playlist = store.getState();
+  $container.innerHTML = '';
+  playlist.forEach(el => {
+    const template = document.createElement('p');
+    template.textContent = el.title;
+    $container.appendChild(template);
+  })
+}
 
-playlist.forEach(el => {
-	const template = document.createElement('p');
-	template.textContent = el.title;
-	$container.appendChild(template);
-})
+render();
+
+function handleChange() {
+  render();
+}
+
+store.subscribe(handleChange)
